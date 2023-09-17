08f48b6afbab54aa2b3583975d363d2ee9999aec
Victor <0327victor@gmail.com>
2023-09-18 01:44:05 +0800

    modified:   Controller_Data/Controller_248_AP.py 	modified:   Controller_Data/Controller_248_Client.py 	modified:   main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import Controller_Data.Controller__249_AP as AP_249
 import Controller_Data.Controller_249_Client as Client_249
+import Controller_Data.Controller_248_AP as AP_248
+import Controller_Data.Controller_248_Client as Client_248
 import threading
 
 threads = []
 threads.append(threading.Thread(target = AP_249.start_Controller_249_AP(), args = ()))     
 threads.append(threading.Thread(target = Client_249.start_Controller_249_Client(), args = ()))    
+threads.append(threading.Thread(target = AP_248.start_Controller_248_AP(), args = ()))     
+threads.append(threading.Thread(target = Client_248.start_Controller_248_Client(), args = ()))    
 for i in range(len(threads)):
     threads[i].start()
