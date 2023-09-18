67145686233b287b8577c0a09c8543e58bbc5ee5
Victor <0327victor@gmail.com>
2023-09-19 00:59:51 +0800

    modified:   Data_Analysis/wifi_client_filter_out_device.py

--- Data_Analysis/wifi_client_filter_out_device.py
+++ Data_Analysis/wifi_client_filter_out_device.py
@@ -15,21 +15,21 @@ import time
 import  Mongo.mongo  as mongo
 
 
-# 提取原始数据
+# 提取原始數據
 raw_data =  mongo.WIFI_FindData('AP_test','April_Client')
 print("ok")
 
 
-# 在 Python 中进行处理和判断，找到每个用户连接了不同 ap_identifier 种类数量最多的设备
+# 在 Python 中進行處理和判断，找到每个用户連接了不同 ap_identifier 種類數量最多的設備
 user_devices = {}
 
 for document in raw_data:
-    # 获取用户名、设备 MAC 和 AP 标识符
+    # 或取用户名、設備 MAC 和 AP 標示符
     user_name = document.get("client_user_name", "")
     device_mac = document.get("sta_mac_address", "")
     ap_identifier = document.get("ap_name", "")
 
-    # 检查用户名中是否包含@，然后进行相应的处理
+    # 檢查用户名中是否包含@，然后進行相應的處理
     if '@' in user_name:
         user_name = user_name.split('@')[0].lower()
     else:
@@ -41,10 +41,10 @@ for document in raw_data:
         user_devices[key] = {"user_name": user_name, "device_mac": device_mac, "ap_identifiers": set()}
     user_devices[key]["ap_identifiers"].add(ap_identifier)
     
-# 找到每个用户连接了 ap_identifier 种类数量最多的设备
+# 找到每个用户連接了 ap_identifier 種類數量最多的設備
 top_devices = {}
 
-# 考虑同一用户可能只有一个设备的情况
+# 考慮同一用户可能只有一個設備的情况
 for key, data in user_devices.items():
     user_name = data["user_name"]
     if user_name not in top_devices:
@@ -53,7 +53,7 @@ for key, data in user_devices.items():
         top_devices[user_name].append(data)
 
 
-# 找到每个用户连接了不同 ap_identifier 种类数量最多的设备
+# 找到每个用户連接了不同 ap_identifier 種類數量最多的設備
 final_top_devices = []
 
 for user_name, devices in top_devices.items():
@@ -72,26 +72,26 @@ mongo.WIFI_WriteInDB("AP_test","MACaddress_test3" , final_top_devices )
 
 
 
-# 创建一个集合来存储要保留的设备 MAC 地址
+# 創建一个集合来存储要保留的設備 MAC 地址
 set_of_device_macs_to_keep = set()
 
-# 填充要保留的设备 MAC 地址集合
+# 填充要保留的設備 MAC 地址集合
 for top_device in final_top_devices:
     set_of_device_macs_to_keep.add(top_device["device_mac"])
 
-# 创建一个列表来存储要插入新数据库的文档
+# 創建一個列表来存储要插入新數據庫的文檔
 documents_to_insert = []
 
 for document in raw_data:
-    # 获取设备 MAC
+    # 獲取設備 MAC
     device_mac = document.get("sta_mac_address", "")
 
-    # 检查是否需要保留此设备
+    # 檢查是否需要保留此設備
     if device_mac in set_of_device_macs_to_keep:
-        # 将此文档添加到要插入的文档列表中
+        # 將此文檔添加到要插入的文檔列表中
         documents_to_insert.append(document)
 print(documents_to_insert)
-# 插入要保留的文档到新的数据库
+# 插入要保留的文檔到新的數據庫
 mongo.WIFI_WriteInDB("AP_test","MACaddress_test4" , documents_to_insert )
 
 
