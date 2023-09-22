ce4f14954fa746b74c6cd06c0327a5820ab0a5c0
Victor <0327victor@gmail.com>
2023-09-22 22:47:03 +0800

    deleted:    Data_Analysis/data = [4, 5, 6, 5].py

--- Data_Analysis/data = [4, 5, 6, 5].py
+++
@@ -1,8 +0,0 @@
-data = [4, 5, 6, 5]
-target_value = 5
-indices = [index for index, value in enumerate(data) if value == target_value]
-
-if indices:
-    print(f"值 {target_value} 在以下位置找到：{indices}")
-else:
-    print(f"值 {target_value} 未在列表中找到")
