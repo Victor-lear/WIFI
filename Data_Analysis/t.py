878c25bb0140f83f6a079eddf0d3ce8d33444a9a
Victor <0327victor@gmail.com>
2023-10-31 21:32:42 +0800

    deleted:    Data_Analysis/t.py

--- Data_Analysis/t.py
+++
@@ -1,8 +0,0 @@
-import time 
-from datetime import datetime
-from datetime import timedelta
-timestamp=1680278400
-start_time = datetime.fromtimestamp(timestamp)
-timestamp=1680364799
-end_time = datetime.fromtimestamp(timestamp)
-print((end_time-start_time)>timedelta(minutes=20))
